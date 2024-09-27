c213fb246769be948445ef0ee67d6d76208da1ba
dianabukliv <dianabukliv@gmail.com>
2024-09-27 20:42:54 +0300

    end

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import sass from 'vite-plugin-sass';
+import { defineConfig } from 'vite';
+import json from '@rollup/plugin-json'; // Імпортуй плагін для JSON
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sass()], 
-})
+  plugins: [json()], // Додаємо плагін у масив plugins
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/variables.scss";`, // Впевнись, що змінні доступні глобально
+      },
+    },
+  },
+});
